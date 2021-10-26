862221fb27dca210398f0b023839170f975cb7f7
Sukant Hajra <sukant@simspace.com>
2021-10-26 01:36:48 -0500

    REBASE: populating Darwin build

---
+++ system/darwin.nix
@@ -0,0 +1,33 @@
+{ ... }:
+
+{
+  homebrew = import ./homebrew;
+
+  nix.binaryCaches = [
+    "https://haskell-language-server.cachix.org"
+    "https://hydra.iohk.io"
+    "https://niv.cachix.org"
+    "https://nix-community.cachix.org"
+    "https://shajra.cachix.org"
+  ];
+  nix.binaryCachePublicKeys = [
+    "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "niv.cachix.org-1:X32PCg2e/zAm3/uD1ScqW2z/K0LtDyNV7RdaxIuLgQM="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "shajra.cachix.org-1:V0x7Wjgd/mHGk2KQwzXv8iydfIgLupbnZKLSQt5hh9o="
+  ];
+  nix.trustedUsers = [ "root" "@staff" ];
+
+  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent.enableSSHSupport = true;
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
+
+  services.nix-daemon.enable = true;
+
+  system.checks.verifyNixPath = false;
+  system.stateVersion = 4;
+
+  users.nix.configureBuildUsers = true;
+}
--- system/default.nix
+++ system/default.nix
@@ -1,8 +1,9 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 let
   try = builtins.tryEval <simspace-custom>;
   custom = if try.success then [try.value] else [];
+  provided = if pkgs.stdenv.isDarwin then ./darwin.nix else ./linux.nix;
 in {
-  imports = [./provided.nix] ++ custom;
+  imports = [provided] ++ custom;
 }
